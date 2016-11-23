89e9bdc5c7d576ec213b1788beaf728c624ba6f0
ruimcf <ruimcf95@gmail.com>
2016-11-23 21:11:37 +0000

    added dlib and it's face detetion and shape predictor for face (landmarks).

--- expression_rec.py
+++ expression_rec.py
@@ -1,5 +1,7 @@
 import cv2
 import sys
+import dlib
+from skimage import io
 
 
 def main():
@@ -11,28 +13,46 @@ def main():
 
 def image_capture():
     cascPath = sys.argv[1]
+    preditor_path = sys.argv[2]
+    #Face classifier
     faceCascade = cv2.CascadeClassifier(cascPath)
 
     video_capture = cv2.VideoCapture(0)
 
+    predictor_path = sys.argv[2]
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(preditor_path)
+    win = dlib.image_window()
+
     while(True):
         #capture frame-by-frame
         ret, frame = video_capture.read()
+        #faces = face_detetion(frame, faceCascade)
+        faces = detector(frame, 1)
 
-        faces = face_detetion(frame, faceCascade)
+        win.clear_overlay()
+        win.set_image(frame)
 
         #Draw a rectangle around the faces
-        for(x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        for face in faces:
+            shape = predictor(frame, face)
+            #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            win.add_overlay(shape)
+
+        win.add_overlay(faces)
+
 
         #Display the resulting frame
         cv2.imshow('Video', frame)
 
         #Press q to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("pressed q")
             break
+    win.close()
     video_capture.release()
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
 def face_detetion(frame, faceCascade):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
