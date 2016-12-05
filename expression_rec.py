068b179bec902a9697e93cc39828a16491275ae7
ruimcf <ruimcf95@gmail.com>
2016-12-05 00:54:09 +0000

    added dataset treatment and fisher faces algorithm

--- expression_rec.py
+++ expression_rec.py
@@ -1,10 +1,15 @@
 import cv2
 import sys
 import dlib
+import glob
+import random
 from skimage import io
 import numpy as np
 
 emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
+fishface = cv2.createFisherFaceRecognizer()
+
+data = {}
 
 #definition of wrapper classes http://stackoverflow.com/questions/8687885/python-opencv-svm-implementation
 class StatModel(object):
@@ -29,6 +34,21 @@ class SVM(StatModel):
 
 #main----------
 def main():
+    print len(sys.argv)
+    if(len(sys.argv) > 1):
+        #debugging face extration in the dataset and dataset creation
+        if(sys.argv[1] == "1"):
+            for emotion in emotions:
+                check_faces(emotion);
+        #Run fisher faces
+        elif(sys.argv[1] == "2"):
+            metascore = []
+            for i in range(0,10):
+                correct = run_recognizer()
+                print "got {} percent correct".format(correct)
+                metascore.append(correct)
+            print "Mean score: {} percent correct".format(np.mean(metascore))
+    #begin webcam capture
     image_capture()
 
 def image_capture():
@@ -77,7 +97,7 @@ def image_capture():
 
             win.add_overlay(shape)
 
-            print normalized_shape
+            #print normalized_shape
 
 
         win.add_overlay(faces)
@@ -94,6 +114,29 @@ def image_capture():
     video_capture.release()
     #cv2.destroyAllWindows()
 
+def check_faces(emotion):
+    detector = dlib.get_frontal_face_detector()
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    files = glob.glob("sorted_set/%s/*" %emotion)
+    no_face = 0
+    filenumber = 0
+    for f in files:
+        frame = cv2.imread(f)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        if len(faces)<1:
+            no_face+=1
+        for (x,y,w,h) in faces:
+            gray = gray[y:y+h, x:x+w]
+            try:
+                out = cv2.resize(gray, (350, 350))
+                cv2.imwrite("dataset/%s/%s.jpg" %(emotion, filenumber), out)
+                filenumber +=1
+            except:
+                print "imagem que fudeu: ",emotion, " ", f
+    print "No_face: %d" %no_face
+
+
 def face_detetion(frame, faceCascade):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
@@ -105,9 +148,58 @@ def face_detetion(frame, faceCascade):
         )
     return faces
 
-#http://www.paulvangent.com/2016/04/01/emotion-recognition-with-python-opencv-and-a-face-dataset/
-def train_fisher_faces():
-    files = 1
+def train_svm():
+    return
+
+
+#fisher faces implementation http://www.paulvangent.com/2016/04/01/emotion-recognition-with-python-opencv-and-a-face-dataset/
+def get_files(emotion):
+    files = glob.glob("dataset/%s/*" %emotion)
+    print "files: ", files
+    random.shuffle(files)
+    training = files[:int(len(files)*0.8)]
+    prediction = files[-int(len(files)*0.2):]
+    return training, prediction
+
+def make_sets():
+    training_data = []
+    training_labels = []
+    prediction_data = []
+    prediction_labels = []
+    for emotion in emotions:
+        training, prediction = get_files(emotion)
+        for item in training:
+            image = cv2.imread(item)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            training_data.append(gray)
+            training_labels.append(emotions.index(emotion))
+
+        for item in prediction:
+            image = cv2.imread(item)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            prediction_data.append(gray)
+            prediction_labels.append(emotions.index(emotion))
+    return training_data, training_labels, prediction_data, prediction_labels
+
+def run_recognizer():
+    training_data, training_labels, prediction_data, prediction_labels = make_sets()
+
+    print "Training fisher face classifier"
+    print "Size of training set is: ",len(training_data)," images"
+    fishface.train(training_data, np.asarray(training_labels))
+
+    print "predicting classification set"
+    count = 0
+    correct = 0
+    incorrect = 0
+    for image in prediction_data:
+        pred, conf = fishface.predict(image)
+        if pred == prediction_labels[count]:
+            correct += 1
+        else:
+            incorrect += 1
+        count +=1
+    return ((100*correct)/(correct + incorrect))
 
 
 if __name__ == "__main__":
